5794c8b6d970c1651ad54aa42b1800f4c42155c8
jdrichards-pursuit <jdrpursuit@gmail.com>
2023-12-08 10:55:30 -0500

    center heading

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const App = () => {
-  return <div>Basic Pursuit React Starter</div>;
+  return (
+    <div style={{ color: "purple", textAlign: "center" }}>
+      <h1>Deployed Netlify App</h1>
+    </div>
+  );
 };
 
 export default App;
